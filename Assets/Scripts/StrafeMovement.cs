66891e5aa84d1d59969901396bd2a8f30e89cb46
Till W <theasuro@gmail.com>
2017-08-27 16:47:43 +0200

    updated

--- Assets/Scripts/StrafeMovement.cs
+++ Assets/Scripts/StrafeMovement.cs
@@ -3,17 +3,17 @@
 public class StrafeMovement : MonoBehaviour
 {
     [SerializeField]
-    private float accel = 200f;
+    private float accel = 200f;         // How fast the player accelerates on the ground
     [SerializeField]
-    private float airAccel = 200f;
+    private float airAccel = 200f;      // How fast the player accelerates in the air
     [SerializeField]
-    private float maxSpeed = 6.4f;
+    private float maxSpeed = 6.4f;      // Maximum player speed on the ground
     [SerializeField]
-    private float maxAirSpeed = 0.6f;
+    private float maxAirSpeed = 0.6f;   // "Maximum" player speed in the air
     [SerializeField]
-    private float friction = 8f;
+    private float friction = 8f;        // How fast the player decelerates on the ground
     [SerializeField]
-    private float jumpForce = 5f;
+    private float jumpForce = 5f;       // How high the player jumps
     [SerializeField]
     private LayerMask groundLayers;
 
@@ -23,108 +23,110 @@ public class StrafeMovement : MonoBehaviour
     private float lastJumpPress = -1f;
     private float jumpPressDuration = 0.1f;
 	private bool onGround = false;
-
-	private bool jumpKeyPressed = false;
     
-	void Update()
-	{
-		//Set key states
-		if(Input.GetButton("Jump"))
-			{ jumpKeyPressed = true; } else { jumpKeyPressed = false; }
-			
-		if(jumpKeyPressed)
+	private void Update()
+    {
+        print(new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, GetComponent<Rigidbody>().velocity.z).magnitude);
+        if (Input.GetButton("Jump"))
 		{
 			lastJumpPress = Time.time;
 		}
 	}
 
-	void FixedUpdate()
+	private void FixedUpdate()
 	{
-		Vector2 input = new Vector2();
 
-        input.x = Input.GetAxis("Horizontal");
-		input.y = Input.GetAxis("Vertical");
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-		//Friction
-		Vector3 tempVelocity = calculateFriction(GetComponent<Rigidbody>().velocity);
-
-		//Add movement
-		tempVelocity += calculateMovement(input, tempVelocity);		
-		
-		//Apply
-		if(!GetComponent<Rigidbody>().isKinematic)
-		{
-			GetComponent<Rigidbody>().velocity = tempVelocity;
-		}
+        // Get player velocity
+        Vector3 playerVelocity = GetComponent<Rigidbody>().velocity;
+        // Slow down if on ground
+        playerVelocity = CalculateFriction(playerVelocity);
+        // Add player input
+        playerVelocity += CalculateMovement(input, playerVelocity);
+        // Assign new velocity to player object
+		GetComponent<Rigidbody>().velocity = playerVelocity;
 	}
 
-	public Vector3 calculateFriction(Vector3 currentVelocity)
+    /// <summary>
+    /// Slows down the player if on ground
+    /// </summary>
+    /// <param name="currentVelocity">Velocity of the player</param>
+    /// <returns>Modified velocity of the player</returns>
+	private Vector3 CalculateFriction(Vector3 currentVelocity)
 	{
-        onGround = checkGround();
-		float speed = currentVelocity.magnitude; 
+        onGround = CheckGround();
+		float speed = currentVelocity.magnitude;
 
-		//Code from https://flafla2.github.io/2015/02/14/bunnyhop.html
-		if(onGround && !Input.GetButton("Jump") && speed != 0f)
-		{
-			float drop = speed * friction * Time.deltaTime;
-			return currentVelocity * (Mathf.Max(speed - drop, 0f) / speed);
-		}
+        if (!onGround || Input.GetButton("Jump") || speed == 0f)
+            return currentVelocity;
 
-		return currentVelocity;
-	}
-
-	//Do movement input here
-	public Vector3 calculateMovement(Vector2 input, Vector3 velocity)
+        float drop = speed * friction * Time.deltaTime;
+        return currentVelocity * (Mathf.Max(speed - drop, 0f) / speed);
+    }
+    
+    /// <summary>
+    /// Moves the player according to the input. (THIS IS WHERE THE STRAFING MECHANIC HAPPENS)
+    /// </summary>
+    /// <param name="input">Horizontal and vertical axis of the user input</param>
+    /// <param name="velocity">Current velocity of the player</param>
+    /// <returns>Additional velocity of the player</returns>
+	private Vector3 CalculateMovement(Vector2 input, Vector3 velocity)
 	{
-        onGround = checkGround();
+        onGround = CheckGround();
 
-		//Different acceleration values for ground and air
-		float curAccel = accel;
-		if(!onGround)
-			curAccel = airAccel;
+        //Different acceleration values for ground and air
+        float curAccel = accel;
+        if (!onGround)
+            curAccel = airAccel;
 
-		//Ground speed
-		float curMaxSpeed = maxSpeed;
+        //Ground speed
+        float curMaxSpeed = maxSpeed;
 
-		//Air speed
-		if(!onGround)
-			curMaxSpeed = maxAirSpeed;
+        //Air speed
+        if (!onGround)
+            curMaxSpeed = maxAirSpeed;
+        
+        //Get rotation input and make it a vector
+        Vector3 camRotation = new Vector3(0f, camObj.transform.rotation.eulerAngles.y, 0f);
+        Vector3 inputVelocity = Quaternion.Euler(camRotation) *
+                                new Vector3(input.x * curAccel, 0f, input.y * curAccel);
 
-		//Get input and make it a vector
-		Vector3 camRotation = new Vector3(0f, camObj.transform.rotation.eulerAngles.y, camObj.transform.rotation.eulerAngles.z);
-		Vector3 inputVelocity = Quaternion.Euler(camRotation) * new Vector3(input.x * curAccel, 0f, input.y * curAccel);
+        //Ignore vertical component of rotated input
+        Vector3 alignedInputVelocity = new Vector3(inputVelocity.x, 0f, inputVelocity.z) * Time.deltaTime;
 
-		//Ignore vertical component of rotated input
-		Vector3 alignedInputVelocity = new Vector3(inputVelocity.x, 0f, inputVelocity.z) * Time.deltaTime;
-		
-		//Get current velocity
-		Vector3 currentVelocity = new Vector3(velocity.x, 0f, velocity.z);
+        //Get current velocity
+        Vector3 currentVelocity = new Vector3(velocity.x, 0f, velocity.z);
 
-		//How close the current speed to max velocity is (1 = not moving, 0 = at/over max speed)
-		float max = Mathf.Max(0f, 1 - (currentVelocity.magnitude / curMaxSpeed));
+        //How close the current speed to max velocity is (1 = not moving, 0 = at/over max speed)
+        float max = Mathf.Max(0f, 1 - (currentVelocity.magnitude / curMaxSpeed));
 
-		//How perpendicular the input to the current velocity is (0 = 90°)
-		float velocityDot = Vector3.Dot(currentVelocity, alignedInputVelocity);
+        //How perpendicular the input to the current velocity is (0 = 90°)
+        float velocityDot = Vector3.Dot(currentVelocity, alignedInputVelocity);
 
-		//Scale the input to the max speed
-		Vector3 modifiedVelocity = alignedInputVelocity * max;
+        //Scale the input to the max speed
+        Vector3 modifiedVelocity = alignedInputVelocity * max;
 
-		//The more perpendicular the input is, the more the input velocity will be applied
-		Vector3 correctVelocity = Vector3.Lerp(alignedInputVelocity, modifiedVelocity, velocityDot);
+        //The more perpendicular the input is, the more the input velocity will be applied
+        Vector3 correctVelocity = Vector3.Lerp(alignedInputVelocity, modifiedVelocity, velocityDot);
 
-		//Apply jump
-		correctVelocity += getJumpVelocity(velocity.y);
+        //Apply jump
+        correctVelocity += GetJumpVelocity(velocity.y);
 
-		//Return
-		return correctVelocity;
-	}
+        //Return
+        return correctVelocity;
+    }
 
-	private Vector3 getJumpVelocity(float yVelocity)
+    /// <summary>
+    /// Calculates the velocity with which the player is accelerated up when jumping
+    /// </summary>
+    /// <param name="yVelocity">Current "up" velocity of the player (velocity.y)</param>
+    /// <returns>Additional jump velocity for the player</returns>
+	private Vector3 GetJumpVelocity(float yVelocity)
 	{
 		Vector3 jumpVelocity = Vector3.zero;
 
-		//Calculate jump
-		if(Time.time < lastJumpPress + jumpPressDuration && yVelocity < jumpForce && checkGround())
+		if(Time.time < lastJumpPress + jumpPressDuration && yVelocity < jumpForce && CheckGround())
 		{
 			lastJumpPress = -1f;
 			jumpVelocity = new Vector3(0f, jumpForce - yVelocity, 0f);
@@ -132,21 +134,15 @@ public class StrafeMovement : MonoBehaviour
 
 		return jumpVelocity;
 	}
-
-	public bool getJumpKeyPressed()
-	{
-		return jumpKeyPressed;
-	}
 	
-	public bool checkGround()
+    /// <summary>
+    /// Checks if the player is touching the ground. This is a quick hack to make it work, don't actually do it like this.
+    /// </summary>
+    /// <returns>True if the player touches the ground, false if not</returns>
+	private bool CheckGround()
 	{
         Ray ray = new Ray(transform.position, Vector3.down);
         bool result = Physics.Raycast(ray, GetComponent<Collider>().bounds.extents.y + 0.1f, groundLayers);
         return result;
-	}
-    
-	private float getVelocity()
-	{
-		return Vector3.Magnitude(GetComponent<Rigidbody>().velocity);
 	}
 }
